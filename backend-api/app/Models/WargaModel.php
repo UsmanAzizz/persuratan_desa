284eb2d85dcab7d4cd05eaef2aac32ca4da04138
UsmanAzizz <salafiesp@gmail.com>
2026-07-05 19:19:26 +0700

    Fix: add missing fields to allowedFields in WargaModel

--- backend-api/app/Models/WargaModel.php
+++ backend-api/app/Models/WargaModel.php
@@ -11,7 +11,7 @@ class WargaModel extends Model
     protected $useAutoIncrement = false;
     protected $returnType       = 'array';
     protected $useSoftDeletes   = false;
-    protected $allowedFields    = ['nik', 'no_kk', 'nama_lengkap', 'alamat', 'rt', 'rw', 'created_at'];
+    protected $allowedFields    = ['nik', 'no_kk', 'nama_lengkap', 'alamat', 'rt', 'rw', 'tempat_lahir', 'tanggal_lahir', 'jenis_kelamin', 'agama', 'pekerjaan', 'status_perkawinan', 'dusun', 'created_at'];
 
     protected $useTimestamps = true;
     protected $createdField  = 'created_at';
