9592fafc13c878aa242a1082b6292bb38ed2dadc
UsmanAzizz <salafiesp@gmail.com>
2026-07-30 09:52:09 +0700

    fix: remove deprecated webVersionCache and add memory flags for puppeteer to fix WA Gateway hanging on Linux VPS

--- wa-gateway/index.js
+++ wa-gateway/index.js
@@ -11,11 +11,16 @@ const client = new Client({
     authStrategy: new LocalAuth(), // Saves session to .wwebjs_auth folder
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    },
-    webVersionCache: {
-        type: 'remote',
-        remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html'
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
